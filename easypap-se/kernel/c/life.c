fb43addd4d10d5cc58e7ccadcc146071941e11f3
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-19 21:37:43 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -46,14 +46,14 @@ void init_has_changed() {
 }
 
 void store_change() {
-  int tmp_x;
-  int tmp_y;
+  int *tmp_x;
+  int *tmp_y;
   for (int i = 0; i < DIM/TILE_W; i++) {
-    tmp_x = after_change_x[i];
+    *tmp_x = after_change_x[i];
     before_change_x[i] = tmp_x;
   }
   for (int j = 0; j < DIM/TILE_H; j++) {
-    tmp_y = after_change_y[j];
+    *tmp_y = after_change_y[j];
     before_change_y[j] = tmp_y;
   }
 }
