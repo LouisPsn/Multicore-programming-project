dd0a8cdf6df76ae600a0949e5834b36d94b0ff3c
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-19 22:07:05 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -275,7 +275,7 @@ unsigned life_compute_omp (unsigned nb_iter)
     unsigned change = 0;
 
     printf("\n");
-    #pragma omp parallel for collapse(2) schedule(dynamic)
+    #pragma omp parallel for schedule(dynamic)
     for (int y = 0; y < DIM; y += TILE_H) {
       for (int x = 0; x < DIM; x += TILE_W) {
         check_change = do_tile (x, y, TILE_W, TILE_H, omp_get_thread_num());
