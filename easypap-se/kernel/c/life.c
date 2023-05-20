40bb002323b245e3292919c359c3c4fc295e6343
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-20 12:09:04 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -50,7 +50,7 @@ void store_change() {
   // int tmp_y = 0;
   // strncpy(before_change, after_change, DIM/TILE_W*DIM/TILE_H);
   for (int i = 0; i < DIM/TILE_W*DIM/TILE_H; i++) {
-    *tmp = after_change[i];
+    tmp = after_change[i];
     before_change[i] = tmp;
   }
   // for (int j = 0; j < DIM/TILE_H; j++) {
