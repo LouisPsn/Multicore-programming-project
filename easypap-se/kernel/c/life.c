3add3b2085dc29cb3b8c1f01f77af61f718939aa
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-20 12:03:13 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -14,10 +14,10 @@ typedef unsigned cell_t;
 
 static cell_t *_table = NULL, *_alternate_table = NULL;
 
-static char restrict *before_change;
+char restrict *before_change;
 // static int *before_change_y;
 
-static char restrict *after_change;
+char restrict *after_change;
 // static int *after_change_y;
 
 
