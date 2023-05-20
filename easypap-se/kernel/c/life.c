d1525b060f17ed6ae987182ce2145b75342d1352
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-20 15:53:11 +0200

    Maraboutage

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -173,7 +173,7 @@ int life_do_tile_sparse (int x, int y, int width, int height)
 
   int i = x/TILE_W;
 
-  if (i < 180 || i > 208) {
+  if (i < 190 || i > 220) {
     int j = y/TILE_H;
 
     long pos = i + j*TILE_W;
