eabba0625e26fc5a7a5fd2aea2ac9c28abf11bf4
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-20 15:27:42 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -24,7 +24,7 @@ static char *before_change;
 void init_has_changed() {
 
   if (before_change == NULL) {
-    before_change = malloc(sizeof(char)*(DIM/TILE_W)*(DIM/TILE_H) + 150000);
+    before_change = malloc(sizeof(char)*(DIM/TILE_W)*(DIM/TILE_H));
   }
   // if (before_change_y == NULL) {after_change[i];
   //   before_change_y = (int*)malloc(sizeof(int)*DIM/TILE_H);
