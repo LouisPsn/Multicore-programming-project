a9b3ce4630b156992e1c2abe06123380a6ec5797
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-20 13:27:03 +0200

    Debug

--- easypap-se/kernel/c/life.c
+++ easypap-se/kernel/c/life.c
@@ -169,35 +169,38 @@ int life_do_tile_sparse (int x, int y, int width, int height)
 {
   int change = 0;
 
-  int change_neigh = 1;
+  int change_neigh = 0;
 
-  int pos = x/TILE_W + (y/TILE_H)*TILE_W;
-  int pos_1 = x/TILE_W + 1 + (y/TILE_H)*TILE_W;
-  int pos_2 = x/TILE_W - 1 + (y/TILE_H)*TILE_W;
-  int pos_3 = x/TILE_W + (y/TILE_H + 1)*TILE_W;
-  int pos_4 = x/TILE_W + (y/TILE_H - 1)*TILE_W;
+  int i = x/TILE_W;
+  int j = y/TILE_H;
+
+  int pos = i + j*TILE_W;
+  int pos_1 = i + 1 + j*TILE_W;
+  int pos_2 = i - 1 + j*TILE_W;
+  int pos_3 = i + (j + 1)*TILE_W;
+  int pos_4 = i + (j - 1)*TILE_W;
   
-  if (pos >= 0 && pos < DIM/TILE_W*DIM/TILE_H) {
+  if (pos >= 0 && pos < (DIM/TILE_W)*(DIM/TILE_H)) {
     if (before_change[pos] == 1) {
       change_neigh = 1;
     }
   }
-  if (pos_1 >= 0 && pos_1 < DIM/TILE_W*DIM/TILE_H) {
+  if (pos_1 >= 0 && pos_1 < (DIM/TILE_W)*(DIM/TILE_H)) {
     if (before_change[pos_1] == 1) {
       change_neigh = 1;
     }
   }
-  if (pos_2 >= 0 && pos_2 < DIM/TILE_W*DIM/TILE_H) {
+  if (pos_2 >= 0 && pos_2 < (DIM/TILE_W)*(DIM/TILE_H)) {
     if (before_change[pos_2] == 1) {
       change_neigh = 1;
     }
   }
-  if (pos_3 >= 0 && pos_3 < DIM/TILE_W*DIM/TILE_H) {
+  if (pos_3 >= 0 && pos_3 < (DIM/TILE_W)*(DIM/TILE_H)) {
     if (before_change[pos_3] == 1) {
       change_neigh = 1;
     }
   }
-  if (pos_4 >= 0 && pos_4 < DIM/TILE_W*DIM/TILE_H) {
+  if (pos_4 >= 0 && pos_4 < (DIM/TILE_W)*(DIM/TILE_H)) {
     if (before_change[pos_4] == 1) {
       change_neigh = 1;
     }
