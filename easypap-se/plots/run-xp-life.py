1c2de0e4ad933ccd18ec769285229464a6e9e5f2
Pierson Louis <lpierson002@enseirb-matmeca.fr>
2023-05-21 22:08:17 +0200

    Graph program

--- easypap-se/plots/run-xp-life.py
+++ easypap-se/plots/run-xp-life.py
@@ -25,7 +25,7 @@ ompenv["OMP_PLACES="] = ["cores", "threads"]
 
 nbrun = 4
 # Lancement des experiences
-execute('./run ', ompenv, options, nbrun, verbose=True, easyPath=".")
+execute('../run ', ompenv, options, nbrun, verbose=True, easyPath=".")
 
 # Lancement de la version seq avec le nombre de thread impose a 1
 options["-v "] = ["seq"]
